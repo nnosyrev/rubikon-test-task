d574056520b59176ef9f61003f9d6d075ce6edf5
Nick <nick@example.com>
2025-12-09 16:40:54 +0300

    Унёс число в константу

--- src/Services/AmoCRM/AmoCRMLeads.php
+++ src/Services/AmoCRM/AmoCRMLeads.php
@@ -8,6 +8,8 @@ use App\Config\Config;
 
 final class AmoCRMLeads extends AmoCRMAbstract
 {
+    private const CHANGE_STATUS_TO_WAITING_PRICE_FROM = 5001;
+
     public function changeStatusToWaiting(): void
     {
         $leadsService = $this->getApiClient()->leads();
@@ -17,7 +19,7 @@ final class AmoCRMLeads extends AmoCRMAbstract
             'status_id' => Config::get('AMOCRM_REQUEST_STATUS_ID'),
             'pipeline_id' => Config::get('AMOCRM_PIPELINE_ID')
         ]]);
-        $filter->setPrice((new BaseRangeFilter)->setFrom(5001)->setTo(PHP_INT_MAX));
+        $filter->setPrice((new BaseRangeFilter)->setFrom(self::CHANGE_STATUS_TO_WAITING_PRICE_FROM)->setTo(PHP_INT_MAX));
         $filter->setLimit(Config::get('AMOCRM_LEADS_LIMIT'));
 
         $leadsCollection = $leadsService->get($filter);
